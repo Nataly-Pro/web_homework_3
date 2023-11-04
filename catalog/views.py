9210a57358da362171824c028ea31b96bd2d1041
natalya tomova <natalyayurevna19@gmail.com>
2023-11-04 16:26:15 +1000

    Добавлен контроллер, url для шаблона product

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    product_list = Product.objects.all()
+    context = {
+        'object_list': product_list,
+        'title': 'Каталог'
+    }
+    return render(request, 'catalog/home.html', context)
 
 
 def contacts(request):
@@ -11,4 +18,16 @@ def contacts(request):
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         print(f'{name} (тел. {phone}): {message}')
-    return render(request, 'catalog/contacts.html')
+
+    context = {
+        'title': 'Контакты'
+    }
+    return render(request, 'catalog/contacts.html', context)
+
+
+def product(request, pk):
+
+    context = {
+        'object': Product.objects.get(pk=pk),
+    }
+    return render(request, 'catalog/product.html', context)
