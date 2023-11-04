e946da569491a057114eed600d1626ae088f505f
natalya tomova <natalyayurevna19@gmail.com>
2023-11-04 16:21:16 +1000

    корректирована структура таблицы product (параметры полей price, preview, create_date, last_mod_date)

--- catalog/models.py
+++ catalog/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=50, verbose_name='наименование')
+    name = models.CharField(max_length=100, verbose_name='наименование')
     text = models.TextField(verbose_name='описание')
     preview = models.ImageField(upload_to='preview/', verbose_name='изображение (превью)', blank=True)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
-    price = models.IntegerField(verbose_name='цена за покупку')
-    create_date = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
-    last_mod_date = models.DateTimeField(auto_now=True, verbose_name='дата последнего изменения')
+    price = models.CharField(max_length=10, verbose_name='цена за покупку')
+    create_date = models.DateField(auto_now_add=True, verbose_name='дата создания')
+    last_mod_date = models.DateField(auto_now=True, verbose_name='дата последнего изменения')
 
     def __str__(self):
-        return f'{self.name}, {self.price}, {self.create_date}'
+        return self.name
 
     class Meta:
         verbose_name = 'продукт'
