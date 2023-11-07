845bbb5db682a61c16c9a21ff11bb44cc9313b66
natalya tomova <natalyayurevna19@gmail.com>
2023-11-08 09:02:52 +1000

    Зарегистрирована модель Blog

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from catalog.models import Product, Category
+from catalog.models import Product, Category, Blog
 
 
 @admin.register(Product)
@@ -11,3 +11,7 @@ class ProductAdmin(admin.ModelAdmin):
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name',)
+
+@admin.register(Blog)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'content', 'is_published',)
