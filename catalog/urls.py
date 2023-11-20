02440e69a35fa18e7ebbff03721ce51d1bcb8f50
natalya tomova <natalyayurevna19@gmail.com>
2023-11-20 22:32:51 +1000

    Реализовано отображение категорий и через них продуктов, изменено содержание шаблонов

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,16 +1,21 @@
 from django.urls import path
 
 from catalog.apps import MainConfig
-from catalog.views import ProductCreateView, ProductListView, ProductDetailView, ContactsView, BlogCreateView, \
-    BlogListView, BlogUpdateView, BlogDeleteView, BlogDetailView
+from catalog.views import ProductCreateView, ProductDetailView, ContactsView, BlogCreateView, \
+    BlogListView, BlogUpdateView, BlogDeleteView, BlogDetailView, CategoryListView, ProductUpdateView, \
+    HygieneProductListView, SkinProductListView, HomeProductListView
 
 app_name = MainConfig.name
 
 urlpatterns = [
-    path('', ProductListView.as_view(), name='home'),
+    path('', CategoryListView.as_view(), name='home'),
+    path('products/?category=5', SkinProductListView.as_view(), name='skin_products'),
+    path('products/?category=6', HygieneProductListView.as_view(), name='hygiene_products'),
+    path('products/?category=7', HomeProductListView.as_view(), name='home_products'),
     path('create_product/', ProductCreateView.as_view(), name='create_product'),
+    path('update_product/<int:pk>/', ProductUpdateView.as_view(), name='update_product'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_view'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
     path('blog_create/', BlogCreateView.as_view(), name='blog_create'),
     path('blog_list/', BlogListView.as_view(), name='blog_list'),
     path('blog_detail/<int:pk>/', BlogDetailView.as_view(), name='blog_view'),
