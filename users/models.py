e7cbfd0ad0f6fa6dcd6bde8032467e052497e35c
natalya tomova <natalyayurevna19@gmail.com>
2023-11-28 15:26:58 +1000

    Добавлена модель User

---
+++ users/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+
+    username = None
+    email = models.EmailField(unique=True, verbose_name='почта')
+    avatar = models.ImageField(upload_to='user/', verbose_name='avatar')
+    phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
+    country = models.CharField(max_length=30, verbose_name='страна', **NULLABLE)
+
+    is_active = models.BooleanField(default=False, verbose_name='активный пользователь')
+    verify_code = models.CharField(max_length=12, verbose_name='код верификации', **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
